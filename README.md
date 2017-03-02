c0b94b78f6b1da401724f9179fa881d9fe36cdd6
Luiz Toni <luiztoni@outlook.com>
2017-03-02 14:57:37 -0300

    Atualizações

--- README.md
+++ README.md
@@ -1,19 +1,13 @@
 # Site Engenhe Jr
 
-<p>A estrutura do site será esta.</p> 
-<p>Muitas mudanças ainda serão realizadas.</p>
-<p>O video do header é apenas um exemplo, o marketing irá produzir um curto video com cenas da engenhe.</p>
-
+<p>A estrutura do site está quase que totalmente pronta.</p>
+<p>Poucas mudanças ainda serão realizadas.</p>
 
 ## Pendencias:
 <ul>
-    <li>Realizar alterações no estilo e corrigir imperfeições na visualização em dispositívos móveis</li>
-    <li>Criar animações de entrada para ítens em lista integrado ao scrollspy</li>
-    <li>Substituir referencias de bibliotecas para arquivos locais</li>
-    <li>adição do paralax em algumas seções</li>
-    <li>implementação da seção parceiros</li>
-    <li>alterar estrutura do rodapé</li>
-    <li>Inserir conteúdo em diversos espaços</li>
+    <li>Email (Implementado, mas sujeito a mudanças)</li>
+    <li>Algumas alterações em determinados serviços</li>
+    <li>Finalizar Portfólio</li>
 </ul>
 
 ### Folha de estilos
@@ -21,3 +15,19 @@
 
 ### Scripts do site
 <code>/js/scripts.js</code>
+
+### Sobre a implementação do envio de Email
+
+- O arquivo `/mail/contact_me.php` contém o script de envio de emails, ele não deve ser posto no servidor do Github pois irá deixar exposto a senha e email utilizado no processo. Utilizaremos aqui, um servidor smtp grande do mercado.
+
+- Para o servidor PHP é necessário subir os arquivos dos diretórios (esses arquivos são dispensáveis no Github):
+  - `/vendor/composer/`
+  - `/vendor/phpmailer/`
+  - `/vendor/autoload.php`
+  - `/mail/contact_me.php`
+
+- O código Javascript encontra-se em: `/js/scripts.js`
+
+- Sugiro a criação de um email no [Outlook](https://outlook.live.com/) para o **envio** dos emails, pois o serviço do gmail é bastante burocrático no quesito segurança. Normalmente os emails recebidos irão parar na caixa de *spam*, contudo marca-lo como não *spam* resolve o problema.
+
+- A configuração de conta de email e senha para o envio encontra-se no arquivo `/mail/contact_me.php`.
--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,26 +1,71 @@
 <?php
-// Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-	echo "No arguments Provided!";
-	return false;
-   }
-	
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
-	
-// Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
-return true;			
+require '../vendor/autoload.php';
+require '../vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
+
+    // Only process POST reqeusts.
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Get the form fields and remove whitespace.
+        $name = strip_tags(trim($_POST["name"]));
+		$name = str_replace(array("\r","\n"),array(" "," "),$name);
+        $phone = strip_tags(htmlspecialchars($_POST['phone']));
+        $subject = strip_tags(htmlspecialchars($_POST['subject']));
+        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+        $message = trim($_POST["message"]);
+        // Check that data was sent to the mailer.
+        if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            // Set a 400 (bad request) response code and exit.
+            http_response_code(400);
+            echo "Oops! Houve um problema com sua submissão, por favor complete o formulário e tente novamente!";
+            exit;
+        }
+
+        // Set the email subject.
+        $subject = "Novo contato de $name: $subject";
+        // Build the email content.
+        $email_content = "Nome: $name\n";
+        $email_content .= "Email: $email\n\n";
+        $email_content .= "Telefone: $phone\n\n";
+        $email_content .= "Mensagem:\n$message\n";
+
+        $mail = new PHPMailer();
+        //Tell PHPMailer to use SMTP
+        $mail->isSMTP();
+        //Enable SMTP debugging
+        // 0 = off (for production use)
+        // 1 = client messages
+        // 2 = client and server messages
+        $mail->SMTPDebug = 0;
+        $mail->Debugoutput = 'html';
+        $mail->CharSet = 'UTF-8';
+        $mail->Host = 'smtp.live.com';
+        $mail->Port = 587;
+        $mail->SMTPSecure = 'tls';
+        $mail->SMTPAuth = true;
+        //Username to use for SMTP authentication - use full email address for gmail
+        $mail->Username = "engenhejr@outlook.com";
+        //Password to use for SMTP authentication
+        $mail->Password = "senhasuper";
+
+        $mail->setFrom($email, $name);
+        $mail->addAddress('engenhejr@gmail.com', 'Engenhe Jr.');     // Add a recipient
+        $mail->isHTML(false);                                  // Set email format to HTML
+
+        $mail->Subject = $subject;
+        $mail->Body    = $email_content;
+
+        //Send the email.
+        if ($mail->send()) {
+            // Set a 200 (okay) response code.
+            http_response_code(200);
+            echo "Obrigado! Sua mensagem foi envida com sucesso.";
+        } else {
+            // Set a 500 (internal server error) response code.
+            http_response_code(500);
+            echo "Oops! $mail->ErrorInfo";
+        }
+    } else {
+        // Not a POST request, set a 403 (forbidden) response code.
+        http_response_code(403);
+        echo "Não é possível fazer essa operação, tente novamente.";
+    }
 ?>
